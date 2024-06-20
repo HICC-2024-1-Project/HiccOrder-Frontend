47b93e20e186e696e3d14caa9c82f3fa25fb20fc
DESKTOP-5DKM4TT\JunhoKim <boy814@naver.com>
2024-06-20 21:52:48 +0900

    feat: add request function with wany

---
+++ src/public/modules/request.mjs
@@ -0,0 +1,58 @@
+import apiEnv from "../static/api.env.mjs";
+// Example POST method implementation:
+async function request(path = "", data = {}, method, contentType) {
+  // Default options are marked with *
+  url = apiEnv.baseUri + "/api/" + path;
+  const header = {
+    "Content-Type": contentType,
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+  };
+  if (localStorage.accessToken) {
+    header.Authorization = localStorage.accessToken;
+  }
+  try {
+    const response = await fetch(url, {
+      method: method, // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: header,
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+  } catch (error) {
+    if (response.status == 403) {
+      try {
+        const json = await response.json();
+        if (json.messages[0].message == "Token is invalid or expired") {
+          const newToken = request(
+            (path = "auth/refresh/"),
+            (data = {
+              refresh: localStorage.refreshToken,
+            }),
+            (method = "post"),
+            (contentType = "application/json")
+          )
+            .then((resp) => {
+              localStorage.accessToken = resp.accessToken;
+              return request(path, data, method, contentType);
+            })
+            .catch((error) => {
+              throw error;
+            });
+        }
+      } catch (error) {
+        throw error;
+      }
+    }
+    throw error;
+  }
+  if (contentType == "text/plain") {
+    return response.text();
+  } else if (contentType == "application/json") {
+    return response.json();
+  }
+
+  return response.blob();
+}
