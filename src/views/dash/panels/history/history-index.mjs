465115750a8ff17f1ca599c4a5d45dcad8cfa6ea
LAPTOP-GD10OHNE\user <goljeol@gmail.com>
2024-08-09 03:18:10 +0900

    feat: 주문기록 확인 페이지 기능 추가(오름차순, 내림차순)

--- src/views/dash/panels/history/history-index.mjs
+++ src/views/dash/panels/history/history-index.mjs
@@ -2,91 +2,102 @@ import { APIGetRequest, APIPostRequest } from '/modules/api.mjs';
 
 const tableInfo = await APIGetRequest(`booth/${localStorage.booth}/table/`);
 const menuInfo = await APIGetRequest(`booth/${localStorage.booth}/menu/`);
-const tables = await APIGetRequest(`booth/${localStorage.booth}/table/`);
-
-var orders = [];
-for (const table of tables) {
-  const orderList = await APIGetRequest(`booth/${localStorage.booth}/order/${table.id}/`)
-  .catch((error) => {
-    console.log(error);
-    return 0;
-  });
-
-  if(orderList) {
-    for (const order of orderList) {
-      orders.push(order);
-    }
-  }
-}
-
-/*
-document.addEventListener('DOMContentLoaded', function () {
-  const orderTableBody = document.querySelector('.order-table tbody');
-
-  // (로컬)저장된 테이블 목록 로드
-  const savedTables = JSON.parse(localStorage.getItem('tables')) || [];
-
-  // 서버 or 로컬 저장소에서 저장된 주문 기록 데이터 가져오기
-
-  // 주문기록을 테이블에 추가하는 함수 addOrderRecord
-  function addOrderRecord(record) {
-    // 새로운 tr 행 생성
-    const row = document.createElement('tr');
-
-    // 새로운 행 내부 요소HTML을
-    // 주문 기록 데이터(메뉴, 테이블 번호, 주문시각, 수량, 결제여부, 결제수단)로 채움
-    row.innerHTML = `   
-            <td>${record.menu}</td>
-            <td>${record.tableNumber}</td>
-            <td>${record.orderTime}</td>
-            <td>${record.quantity}</td>
-            <td>${record.paid}</td>
-            <td>${record.paymentMethod}</td>
-        `;
-
-    // 새로 만든 행을 추가
-    orderTableBody.appendChild(row);
-  }
-
-  // 페이지 로드 시 저장된 주문 기록을 테이블에 추가
-  orderRecords.forEach(addOrderRecord);
-
-  // 사용자에서 주문을 했을 때 이걸
-  // 관리자형(메뉴, 테이블번호, 수량, 결제여부, 결제수단)으로 변환하는 과정이 필요함
-
-  // 현재 시간 기록하는 함수
-  // const now = new Date();
-  // const orderTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-});
-*/
+localStorage.setItem('orderKey', 'timestamp');
+localStorage.setItem('orderSort', 'asc');
 
 const MAIN = {
   async displayHistory(orders) {
-    document.querySelector('.history-table')
-    .innerHTML = '<thead><tr><th>주문 시간</th><th>테이블 이름</th><th>메 뉴 명</th><th>개 수</th><th>가 격</th><th>결제 여부</th></tr></thead>';
-
     let html = '';
+    html += `<thead>`;
+    html += `  <tr>`;
+    html += `    <th>`;
+    html += `      <span class="text">주문 시간</span>`;
+    if (localStorage.getItem('orderKey') === 'timestamp') {
+      html += `      <span class="arrow">`;
+      if (localStorage.getItem('orderSort') === 'asc') html += `keyboard_arrow_up</span>`;
+      else html += `keyboard_arrow_down</span>`;
+    }
+    else html += `      <span class="arrowDisable">keyboard_arrow_down</span>`;
+    html += `    </th>`;
+    html += `    <th>`;
+    html += `      <span class="text">테이블 이름</span>`;
+    if (localStorage.getItem('orderKey') === 'table_id') {
+      html += `      <span class="arrow">`;
+      if (localStorage.getItem('orderSort') === 'asc') html += `keyboard_arrow_up</span>`;
+      else html += `keyboard_arrow_down</span>`;
+    }
+    else html += `      <span class="arrowDisable">keyboard_arrow_down</span>`;
+    html += `    </th>`;
+    html += `    <th>`;
+    html += `      <span class="text">메 뉴 명</span>`;
+    if (localStorage.getItem('orderKey') === 'menu_id') {
+      html += `      <span class="arrow">`;
+      if (localStorage.getItem('orderSort') === 'asc') html += `keyboard_arrow_up</span>`;
+      else html += `keyboard_arrow_down</span>`;
+    }
+    else html += `      <span class="arrowDisable">keyboard_arrow_down</span>`;
+    html += `    </th>`;
+    html += `    <th>`;
+    html += `      <span class="text">개  수</span>`;
+    if (localStorage.getItem('orderKey') === 'quantity') {
+      html += `      <span class="arrow">`;
+      if (localStorage.getItem('orderSort') === 'asc') html += `keyboard_arrow_up</span>`;
+      else html += `keyboard_arrow_down</span>`;
+    }
+    else html += `      <span class="arrowDisable">keyboard_arrow_down</span>`;
+    html += `    </th>`;
+    html += `    <th>`;
+    html += `      <span class="text">가  격</span>`;
+    html += `      <span class="arrowDisable">keyboard_arrow_down</span>`;
+    html += `    </th>`;
+    html += `    <th>`;
+    html += `      <span class="text">결제 여부</span>`;
+    if (localStorage.getItem('orderKey') === 'state') {
+      html += `      <span class="arrow">`;
+      if (localStorage.getItem('orderSort') === 'asc') html += `keyboard_arrow_up</span>`;
+      else html += `keyboard_arrow_down</span>`;
+    }
+    else html += `      <span class="arrowDisable">keyboard_arrow_down</span>`;
+    html += `    </th>`;
+    html += `  </tr>`;
+    html += `</thead>`;
+    document.querySelector('.history-table').innerHTML = html;
+
+    html = '';
     const historyElement = document.createElement('tbody');
     for (const order of orders) {
       html = this.getHistoryElement(order);
       historyElement.innerHTML += html;
       document.querySelector('.history-table').appendChild(historyElement);
     }    
+
+    const headers = document.querySelectorAll('.history-table th'); // 모든 th 선택
+    for (const header of headers) {
+      header.addEventListener('click', () => {
+        const text = header.querySelector('.text').textContent.trim(); // 택스트 내용 가져오기
+
+        if(text === '주문 시간') localStorage.setItem('orderKey', 'timestamp');
+        else if(text === '테이블 이름') localStorage.setItem('orderKey', 'table_id');
+        else if(text === '메 뉴 명') localStorage.setItem('orderKey', 'menu_id');
+        else if(text === '개  수') localStorage.setItem('orderKey', 'quantity');
+        else if(text === '가  격') alert('얘는 아직 미구현!');
+        else localStorage.setItem('orderKey', 'state');
+
+        if (localStorage.getItem('orderSort') === 'asc') localStorage.setItem('orderSort', 'desc');
+        else localStorage.setItem('orderSort', 'asc');
+
+        const sortedOrders = sortByKey(orders, localStorage.getItem('orderKey'), localStorage.getItem('orderSort'));
+        this.displayHistory(sortedOrders);
+      });
+    }
   },
   // 여기서 그만!!
   getHistoryElement(order) {
     const tableName = this.getTableName(order.table_id);
     const menuName = this.getMenuName(order.menu_id);
     const menuPrice = this.getMenuPrice(order.menu_id);
-
-    console.log(typeof(menuPrice));
-    console.log(typeof(order.quantity));
     const price = menuPrice * order.quantity;
-    console.log(menuPrice);
-    console.log(order.quantity);
-    console.log(price);
 
-    const element = document.createElement('tbody');
     let html = '';
     html += `<tr>`;
     html += `<td>${order.timestamp
@@ -128,8 +139,88 @@ const MAIN = {
   },
 }
 
+function sortByKey(array, key, order) {
+  return array.sort((a, b) => {
+    let x = a[key];
+    let y = b[key];
+    if (typeof x === 'string') {
+        x = x.toLowerCase();
+        y = y.toLowerCase();
+    }
+
+    console.log(a);
+    console.log(key);
+    console.log(x);
+
+    if (order === 'asc') {
+        return x < y ? -1 : x > y ? 1 : 0;
+    } 
+    else 
+    {
+        return x > y ? -1 : x < y ? 1 : 0;
+    }
+  });
+}
+
 async function init() {
+  const tables = await APIGetRequest(`booth/${localStorage.booth}/table/`);
+
+  var orders = [];
+  for (const table of tables) {
+    const orderList = await APIGetRequest(`booth/${localStorage.booth}/order/${table.id}/`)
+    .catch((error) => {
+      console.log(error);
+      return 0;
+    });
+
+    if(orderList) {
+      for (const order of orderList) orders.push(order);
+    }
+  }
+  orders = sortByKey(orders, 'timestamp', 'asc');
+
   MAIN.displayHistory(orders);
 }
 
 init();
+
+/*
+document.addEventListener('DOMContentLoaded', function () {
+  const orderTableBody = document.querySelector('.order-table tbody');
+
+  // (로컬)저장된 테이블 목록 로드
+  const savedTables = JSON.parse(localStorage.getItem('tables')) || [];
+
+  // 서버 or 로컬 저장소에서 저장된 주문 기록 데이터 가져오기
+
+  // 주문기록을 테이블에 추가하는 함수 addOrderRecord
+  function addOrderRecord(record) {
+    // 새로운 tr 행 생성
+    const row = document.createElement('tr');
+
+    // 새로운 행 내부 요소HTML을
+    // 주문 기록 데이터(메뉴, 테이블 번호, 주문시각, 수량, 결제여부, 결제수단)로 채움
+    row.innerHTML = `   
+            <td>${record.menu}</td>
+            <td>${record.tableNumber}</td>
+            <td>${record.orderTime}</td>
+            <td>${record.quantity}</td>
+            <td>${record.paid}</td>
+            <td>${record.paymentMethod}</td>
+        `;
+
+    // 새로 만든 행을 추가
+    orderTableBody.appendChild(row);
+  }
+
+  // 페이지 로드 시 저장된 주문 기록을 테이블에 추가
+  orderRecords.forEach(addOrderRecord);
+
+  // 사용자에서 주문을 했을 때 이걸
+  // 관리자형(메뉴, 테이블번호, 수량, 결제여부, 결제수단)으로 변환하는 과정이 필요함
+
+  // 현재 시간 기록하는 함수
+  // const now = new Date();
+  // const orderTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+});
+*/
